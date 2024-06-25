0a0090fbef2cd8003e31e539f1d2c61825710d1f
victor-nsengiyumva <jerrysyre@gmail.com>
2024-06-25 14:21:42 +0300

    created home screen

---
+++ lib/src/features/game/presentation/home.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:wordquest/src/features/utils/main_button.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('WordQuest'),
+      ),
+      body: CustomScrollView(slivers: [
+        SliverFillRemaining(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10, top: 50),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 300,
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          left: 0,
+                          right: 0,
+                          bottom: 50,
+                          child: Center(
+                            child: Image.asset(
+                                "assets/images/game_character.png",
+                                width: 250,
+                                height: 250),
+                          ),
+                        ),
+                        Positioned(
+                            top: 150,
+                            left: 0,
+                            right: 0,
+                            child: Center(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    border:
+                                        Border.all(color: Color(0xFF082163)),
+                                    borderRadius: BorderRadius.circular(20),
+                                    color: Color.fromARGB(255, 223, 230, 248)),
+                                width: 300,
+                                child: Padding(
+                                  padding: const EdgeInsets.only(
+                                      left: 30, right: 30, bottom: 20, top: 20),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "Hello Victor",
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 20,
+                                            color: Color(0xFF082163)),
+                                      ),
+                                      SizedBox(
+                                        height: 5,
+                                      ),
+                                      Text(
+                                        "Lets see how best you can read my mind by guessing the words I am thinking about.",
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w400,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ))
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 60,
+                  ),
+                  SizedBox(
+                      width: 320,
+                      child: MainButton(text: "Start game", onpressed: () {})),
+                  SizedBox(
+                    height: 30,
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ]),
+    );
+  }
+}
